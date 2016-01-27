d7c25453085ad11a8e0cf23ee6973872b5c4a2cb
App Academy Student <student@appacademy.io>
2016-01-26 18:02:10 -0800

    End of Day

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -18,15 +18,27 @@ class CatsController < ApplicationController
   def create
     @cat = Cat.new(cat_params)
     if @cat.save
-
       redirect_to cats_url
     else
-      #make this not delete all info!
       render :new
     end
+  end
 
-    def edit
+  def edit
+    @cat = Cat.find_by(id: params[:id])
+    unless @cat.nil?
+      render :edit
+    else
+      render :new
+    end
+  end
 
+  def update
+    @cat = Cat.find_by(id: params[:id])
+    if @cat.update(cat_params)
+      redirect_to cats_url
+    else
+      render :edit
     end
   end
 
